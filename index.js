20fcad430f6cf5ba1621aee5ae16f1ae1ca7a114
UberBooster <info@uberbooster.com>
2016-06-23 19:31:06 -0400

    set up deployment to Heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var server = express();
+var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname+'/public'));
 
@@ -8,6 +9,6 @@ server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
-server.listen(8080, function(){
-  console.log('Now listening on port...' + 8080);
+server.listen(port, function(){
+  console.log('Now listening on port...' + port);
 });
